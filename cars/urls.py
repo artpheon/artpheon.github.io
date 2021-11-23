25101afcfc98aa5017126fa567ee9d2d478256fa
artpheon <idcdtokms@gmail.com>
2021-11-23 23:47:50 +0300

    single car page & not finished

--- cars/urls.py
+++ cars/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.cars, name='cars'),
+    path('<int:id>', views.car_detailed, name='car_detailed'),
 ]
--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from . import models
 
 # Create your views here.
 
 def cars(request):
     return render(request, 'cars/cars.html')
+
+def car_detailed(request, id):
+    car = get_object_or_404(models.Car, pk=id)
+    data = {
+        'car': car,
+    }
+    return render(request, 'cars/car_detailed.html', data)
