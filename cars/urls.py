728ede18f03169f271f5fc5afd2152a9ab8998eb
artpheon <idcdtokms@gmail.com>
2021-11-28 14:09:45 +0300

    finished pagination & started search page

--- cars/urls.py
+++ cars/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.cars, name='cars'),
     path('<int:id>', views.car_detailed, name='car_detailed'),
+    path('search', views.search, name='search'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- cars/views.py
+++ cars/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from . import models
 
 # Create your views here.
 
 def cars(request):
     cars = models.Car.objects.order_by('-created_date')
+    paginator = Paginator(cars, 3)
+    page = request.GET.get('page')
+    paged_cars = paginator.get_page(page)
     data = {
-        'cars': cars
+        'cars': paged_cars
     }
     return render(request, 'cars/cars.html', data)
 
@@ -18,3 +22,6 @@ def car_detailed(request, id):
         'car': car,
     }
     return render(request, 'cars/car_detailed.html', data)
+
+def search(request):
+    return render(request, 'cars/search.html')
